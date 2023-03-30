0a4c38f517593bfa384aaf2f960b59c49fcf1064
FrancoSantaCruz <sczfranco@gmail.com>
2023-03-30 13:40:36 -0300

    Optimize Project Structure

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div>
+      Holi
+    </div>
+  );
+}
+
+/*
+
+HTML                JSX
+class               className
+<input>             <input />
+`${variable}`       {variable}
+
+*/
