83d495bdcddcd5652e4548cb078cd58283e8e630
FrancoSantaCruz <sczfranco@gmail.com>
2023-04-01 19:30:03 -0300

    Primer Pre-Entrega Santa Cruz Franco

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
 import './App.css';
-
+import { ItemCount } from './ItemCount/ItemCount';
 import React from 'react';
-
+import { NavBar } from './NavBar/NavBar';
+import { ItemListContainer } from './ItemListContainer/ItemListContainer'
 export const App = () => {
   return (
     <div>
-      Holi
+      <NavBar />
+
+
+      <ItemCount />
+      <ItemListContainer greeting={"PRODUCTOS"}/>
     </div>
   );
 }
 
 /*
+<Saludo mensaje={"Hola, Buenos días!"} traduccion={"Hi, good morning!"}/> 
+
 
 HTML                JSX
 class               className
